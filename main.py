2164ca73291598bdb72be922dc42227e9d213e23
DiegoVivas13 <62918291+DiegoVivas13@users.noreply.github.com>
2020-04-11 22:01:54 -0500

    primera modificacion de la parte 2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-a=float(input("ingrese el numero que quiera elevar: "))
-b=int(input("ingrese el valor del exponente: "))
-def a_power_b(a,b):
-    result=1
-    for number in range(b):
-        result=result*a
-        number=number+1
-    print(result)
-a_power_b(a,b)
+a=1
+while a !=0:
+    a=float(input("ingrese el numero que quiera elevar: "))
+    b=int(input("ingrese el valor del exponente: "))
+    def a_power_b(a,b):
+        result=1
+        for number in range(0,b):
+            result=result*a
+            number=number+1
+        print(result)
+    a_power_b(a,b)
